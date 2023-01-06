3a8f5765be9a37bc56819133337996be1bbd2d04
3o14 <dldnjswn4265@gmail.com>
2023-01-06 13:24:22 +0900

    create method

--- categories/serializers.py
+++ categories/serializers.py
@@ -1,10 +1,16 @@
-
 from rest_framework import serializers
+from .models import Category
 
-
-class CategorySerializer(serializers.Serializer):
-
-    pk = serializers.IntegerField()
-    name = serializers.CharField(required=True)
-    kind = serializers.CharField()
-    created_at = serializers.DateTimeField()
+class CategorySerializer(serializers.Serializer):   
+    pk = serializers.IntegerField(read_only=True)
+    name = serializers.CharField(
+        required=True,
+        max_length=50,
+    )
+    kind = serializers.ChoiceField(
+        choices=Category.CategoryKindChoices.choices,
+    )
+    created_at = serializers.DateTimeField(read_only=True)
+    
+    def create(self, validated_data):
+        return Category.objects.create(**validated_data)
