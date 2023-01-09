5ced838fbc29c8c2191d61e3c926d8fe225cbe4f
3o14 <dldnjswn4265@gmail.com>
2023-01-09 21:11:25 +0900

    APIView로 리팩토링

--- categories/urls.py
+++ categories/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.categories),
-    path("<int:pk>", views.category),
+    # 클래스로 수정했기 때문에
+    # 클래스를 호출한 뒤 as_view()를 호출해야 한다. (그냥 규칙임)
+    path("", views.Categories.as_view()),
+    path("<int:pk>", views.CategoryDetail.as_view()),
 ]
