0d8ececa6cb6e3fef3ad387ad92ac3491423946a
3o14 <dldnjswn4265@gmail.com>
2023-01-10 00:09:52 +0900

    ModelViewSets

--- categories/serializers.py
+++ categories/serializers.py
@@ -8,14 +8,3 @@ class CategorySerializer(serializers.ModelSerializer):
         # fields = 어떤 필드를 보이게 할지 설정
         # 모든 필드 보이게 하기
         fields = "__all__" 
-        
-        # 제외시킬 필드 고르기
-        exclude = (
-            "created_at",
-        )
-        
-        # 포함시킬 필드 고르기
-        include = (
-            "name",
-            "kind",
-        )
